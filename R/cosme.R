4cbb5735bb54a59fff481552d66e2438c7828ace
Brian Keller <bk@bkeller2.com>
2024-02-07 12:07:00 -0600

    Add `fit` and `estimates` functions

--- R/cosme.R
+++ R/cosme.R
@@ -31,3 +31,60 @@ cosme <- function(model, data, ...) {
         class = c("cosme_fit", "environment")
     )
 }
+
+#' Check if `cosme_fit`
+#' @noRd
+is.fit <- function(x) {
+    inherits(x, "cosme_fit")
+}
+
+#' Obtain Estimates from `comse_fit`
+#' @description
+#' A short description...
+#'
+# TODO better description of parameters
+#' @param object a `cosme_model` object
+#' @import lavaan
+#' @export
+estimates <- function(object) {
+    # Check that it is a `cosme_fit`
+    if (!is.fit(object)) {
+        throw_error(c(
+            "x" = "The object is not of class {.cls cosme_fit}"
+        ))
+    }
+    # Return output
+    list(
+        freq  = lavaan::parameterEstimates(object$freq),
+        bayes = lavaan::parameterTable(object$bayes),
+        info  = modlist_summary(object$info$mod_list)
+    )
+}
+
+
+#' Obtain fit statistics from `comse_fit`
+#' @description
+#' A short description...
+#'
+# TODO better description of parameters
+#' @param object a `cosme_model` object
+#' @import semTools lavaan blavaan
+#' @export
+fit <- function(object) {
+    # Check that it is a `cosme_fit`
+    if (!is.fit(object)) {
+        throw_error(c(
+            "x" = "The object is not of class {.cls cosme_fit}"
+        ))
+    }
+    # Return output
+    list(
+        # DOES NOT CURRENTLY WORK DUE TO BUG IN semTools
+        # Need to wait for semTools to update
+        # freq  = list(lavaan::fitmeasures(object$freq), semTools::moreFitIndices(object$freq)),
+        freq  = list(lavaan::fitmeasures(object$freq)),
+        # Takes awhile
+        bayes = list(lavaan::fitMeasures(object$bayes), blavaan::blavFitIndices(object$bayes)),
+        info  = list(NULL) # TODO implement
+    )
+}
--- R/utils.R
+++ R/utils.R
@@ -4,3 +4,36 @@
 throw_error <- function(message, ..., .envir = parent.frame(), .frame = .envir) {
     cli::cli_abort(message, ..., .envir = .envir, .frame = .frame, call = NULL)
 }
+
+
+#' Internal function for producing average estimates
+#' @noRd
+modlist_summary <- function(object) {
+    # Make sure only one model compared
+    if (length(object) != 1) {
+        throw_error(c(
+            "!" = "Incorrect number of models specified."
+        ))
+    }
+    object <- object[[1]]
+
+    # Make sure that there is more than one replication
+    if (length(object) == 0) {
+        throw_error(c(
+            "x" = "Not enough replications were specified."
+        ))
+    }
+    val <- sapply(object, \(.) parameterEstimates(.)$est)
+    out <- cbind(
+        mean = rowMeans(val),
+        sd = apply(val, 1, sd),
+        t(apply(val, 1, quantile))
+    )
+
+    # Set collapsed parameter names as row names
+    rownames(out) <- apply(
+        parameterEstimates(object[[1]])[,c('lhs', 'op', 'rhs')],
+        1, paste0, collapse = ' '
+    )
+    return(out)
+}
